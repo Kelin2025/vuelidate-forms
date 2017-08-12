05dea42f2f4cafc17b9e413743a1c6cd2a5597cb
Anton Kosykh <kelin2025@yandex.ru>
2017-08-12 04:02:57 +0300

    Some minor improvement and fix bug with adding methods

--- src/index.js
+++ src/index.js
@@ -4,6 +4,32 @@ import mapValues from 'lodash/mapValues'
 import { buildObject, createDataFromSchema, createValidationsFromSchema } from './utils'
 
 export default (Vue) => {
+
+  // Generate validations option
+  let methods = {
+    // Set previous state to form
+    reset (name) {
+      if (!get(this, name)) {
+        console.warn(`[Vuelidate form] $forms.reset | Form ${name} not found`)
+        return
+      }
+      set(
+        this,
+        name,
+        createDataFromSchema(this.$options.forms[name])
+      )
+    },
+    // Check if form has errors
+    validate (name) {
+      let validator = get(this.$v, name)
+      if (!validator) {
+        console.warn(`[Vuelidate form] $form.validate() | Validator ${name} not found`)
+        return
+      }
+      return validator.$touch() || !validator.$invalid
+    }
+  }
+
   Vue.mixin({
     data () {
       return !this.$options.forms
@@ -11,31 +37,8 @@ export default (Vue) => {
       : buildObject(this, createDataFromSchema)
     },
     beforeCreate () {
-      if (!this.$options.forms) return {}
-      // Generate validations option
-      this.$options.validations = buildObject(this, createValidationsFromSchema)
-      let methods = {
-        // Set previous state to form
-        reset (name) {
-          if (!get(this, name)) {
-            console.warn(`[Vuelidate form] $forms.reset | Form ${name} not found`)
-            return
-          }
-          set(
-            this,
-            name,
-            createDataFromSchema(this.$options.forms[name])
-          )
-        },
-        // Check if form has errors
-        validate (name) {
-          let validator = get(this.$v, name)
-          if (!validator) {
-            console.warn(`[Vuelidate form] $form.validate() | Validator ${name} not found`)
-            return
-          }
-          return validator.$touch() || !validator.$invalid
-        }
+      if (this.$options.forms) {
+        this.$options.validations = buildObject(this, createValidationsFromSchema)
       }
       // Default $forms.reset(name)
       this.$forms = mapValues(methods, item => item.bind(this))
