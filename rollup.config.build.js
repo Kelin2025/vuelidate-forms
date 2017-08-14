0f6f6e653e67d8b1faaeb3f15f4c49a8a71d5513
Anton Kosykh <kelin2025@yandex.ru>
2017-08-14 03:46:26 +0300

    Added uglify

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -1,16 +1,19 @@
-import babel from 'rollup-plugin-babel';
 import cjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 import resolve from 'rollup-plugin-node-resolve';
+import { minify } from 'uglify-es';
 
 export default {
   entry: 'src/index.js',
   plugins: [
-    babel(),
     cjs(),
-    resolve()
+    babel(),
+    resolve(),
+    uglify({}, minify)
   ],
   format: 'es',
   sourceMap: true,
-  moduleName: 'apicase',
+  moduleName: 'vuelidate-forms',
   dest: 'dist/index.js'
 };
